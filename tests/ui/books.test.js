26c36cea8832de10c9a1e26e774e7fbbacd9b3cf
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2023-12-09 15:24:25 +0200

    Implement UI tests in CI/CD and change the link url in tests

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/books');
+    await page.goto('https://spasina00-retake-exam.onrender.com/books');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
